a529706c76e00f6ef3f6729fb3fbe4cd629d0f04
Braedencraig <braeden.craig@gmail.com>
2022-07-03 11:30:33 -0400

    adasdsaD

--- pages/index.js
+++ pages/index.js
@@ -30,11 +30,12 @@ export default function Home({ homepage }) {
   return (
     <div className="full-screen-video-container">
       <HeadInfo />
-      <video autoPlay loop muted playsinline>
+      <video autoPlay muted loop src={`https://${fields.video.fields.file.url}`}></video>
+      {/* <video autoPlay loop muted playsinline>
         <source src="https://www.w3schools.com/html/mov_bbb.mp4" />
         <source src="https://www.w3schools.com/html/mov_bbb.webm" onError="fallback(parentNode)" />
         <img src="https://www.w3schools.com/html/mov_bbb.gif" />
-      </video>
+      </video> */}
       {/* <video autoPlay loop muted playsinline>
         <source src={`https://${fields.video.fields.file.url}`} type="video/mp4" />
       </video> */}
