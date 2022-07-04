1768b13336c29d1acc42c9d5d356e50e9a3ea0f6
Braedencraig <braeden.craig@gmail.com>
2022-07-04 09:43:24 -0400

    update css

--- pages/talent/[slug].js
+++ pages/talent/[slug].js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import Link from "next/link";
 import TalentBar from "../../components/TalentBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
@@ -58,11 +59,48 @@ export default function TalentDetails({ talent, talentImages }) {
   return (
     <>
       <div className="talent-container-single">
-        <div className="flex-container">
-          <h2>{title}</h2>
+        <div className="mobile-talent">
           <div className="mobile-image">
             <img src={`https://${url}`} alt={description} />
           </div>
+          <h2>{title}</h2>
+          <div className="socials">
+            <a href={instagramurl} className="socials-spacing">
+              <div className="social">
+                <FontAwesomeIcon icon={faInstagram} />
+              </div>
+              <p>{instagramfollowers}</p>
+            </a>
+            <a href={youtubeurl}>
+              <div className="social">
+                <FontAwesomeIcon icon={faYoutube} />
+              </div>
+              <p>{youtubefollowers}</p>
+            </a>
+          </div>
+          <p className="bio">{bio.content[0].content[0].value}</p>
+          <Link href="/talent">
+            <a className="back-to-gallery">
+              <div className="boxes">
+                <div>
+                  <div className="box"></div>
+                  <div className="box"></div>
+                </div>
+                <div>
+                  <div className="box"></div>
+                  <div className="box"></div>
+                </div>
+              </div>
+              <a>BACK TO TALENT GALLERY</a>
+            </a>
+          </Link>
+        </div>
+
+        <div className="flex-container desktop-talent">
+          <h2>{title}</h2>
+          {/* <div className="mobile-image">
+            <img src={`https://${url}`} alt={description} />
+          </div> */}
           <div className="socials">
             <a href={instagramurl} className="socials-spacing">
               <div className="social">
