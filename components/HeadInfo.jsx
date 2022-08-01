a330261789967cf867b100af5fc1036aca5b5257
Braedencraig <braeden.craig@gmail.com>
2022-08-01 16:21:25 -0400

    update favicon

--- components/HeadInfo.jsx
+++ components/HeadInfo.jsx
@@ -5,29 +5,26 @@ function HeadInfo() {
     <Head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="keywords" content="good people only,good people commons,good people studio,good people artist management,the cabin recording,toronto,junction,creative,collective,artists,film" />
-      <meta name="description" content="Creative collective based in Toronto, ON. Hosting artists and entrepreneurs in film, music, design, and tech." />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:title" content="GOOD PEOPLE ONLY" />
-      <meta property="og:description" content="Creative collective based in Toronto, ON. Hosting artists and entrepreneurs in film, music, design, and tech. Desks available for rent. " />
       <meta
-        property="og:image"
-        content="https://pro2-bar-s3-cdn-cf6.myportfolio.com/f37e5fcc-8293-4aba-b0ef-f9139ce82753/7cc800bc-d19e-49bc-ac01-a8543ff25553_rw_600.png?h=bd99fb1f7bb5b6f66a398b08d6521a4a"
+        name="keywords"
+        content="nine four, nine four ent, nine four entertainment, talent management, digital, youtube, youtubers, podcast, podcasts, podcasters, investing, angel investing, brand building, crank game plays, eamon + bec, poudii, jennelle eliana, berleezy, jacksfilms, marki plier, blowback, chappo trap house, zias, pavarti shallow, qanon anonymous"
       />
-      <link
-        rel="icon"
-        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAADUExURUxpcU3H2DoAAAABdFJOUwBA5thmAAAADElEQVQI12NgIA0AAAAwAAHHqoWOAAAAAElFTkSuQmCC"
-      />
-      <link rel="canonical" href="https://goodpeopleonly.com/" />
-      <title>WHAT EVER IT SHOULD BE</title>
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="description" content="Talent Management and Business Partners to the Internet’s Top Creators" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:title" content="Nine Four Entertainment" />
+      <meta property="og:description" content="Talent Management and Business Partners to the Internet’s Top Creators" />
+      <meta property="og:image" content="/favicon-32x32.png" />
+      <link rel="icon" href="/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff"></meta>
+      <link rel="canonical" href="https://ninefourent.com/" />
+      <title>Nine Four Entertainment</title>
+      <link rel="icon" href="/favicon.ico" />
     </Head>
   );
 }
